3e2eb0a129377a091aea67f28e90fcc48ffb89ea
GuiHeinrich <gui.heinrich1@gmail.com>
2024-09-11 11:44:44 -0300

    Add files via upload

---
+++ Fila.java
@@ -0,0 +1,22 @@
+public class Fila {
+    private static Fila instance;
+
+    private Fila(){
+
+    }
+    public void ImprimeDocumento(){
+
+    }
+    public void RemoveDocumento(){
+
+    }
+    public void RemoveTodosDocumentos(){
+
+    }
+    public static Fila getInstance() {
+        if (instance == null) {
+            instance = new Fila();
+        }
+        return instance;
+    }
+}
